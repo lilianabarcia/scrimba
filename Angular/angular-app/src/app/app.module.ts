8cd666a358fc0cefa1d4a53c2bab8275d3247a10
lilianabarcia <lilianabarciap@gmail.com>
2022-10-10 16:31:13 +0100

    Observable subscription and sortService

--- Angular/angular-app/src/app/app.module.ts
+++ Angular/angular-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     CustomersModule,
     SharedModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
