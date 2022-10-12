ed6b891b8c3a71ef54abbc9ed71ff02fd174098f
lilianabarcia <lilianabarciap@gmail.com>
2022-10-12 23:14:53 +0100

    final project

--- Angular/angular-app/src/app/app.module.ts
+++ Angular/angular-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { OrdersModule } from './orders/orders.module';
+
+
 
 
 @NgModule({
@@ -26,3 +29,5 @@ import { OrdersModule } from './orders/orders.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
